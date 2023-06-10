bde903dd49b1dc3f8a3844c92deae1e5ec6570ad
naknamu <jodeldelvalle@gmail.com>
2023-06-10 17:57:26 +0800

    Added dynamic sitemap and edited image config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
   reactStrictMode: true,
   images: {
-    domains: ["pub-9c57eec207bf46e8acf2f916d8ea29e7.r2.dev"],
-    unoptimized: true
+    domains: ["pub-9c57eec207bf46e8acf2f916d8ea29e7.r2.dev"]
   },
   compiler: {
     // Enables the styled-components SWC transform
---
+++ pages/server-sitemap.xml/index.js
@@ -0,0 +1,21 @@
+// pages/server-sitemap.xml/index.tsx
+import { getServerSideSitemapLegacy } from 'next-sitemap'
+import config from "@/config/config";
+
+export const getServerSideProps = async (ctx) => {
+  // Method to source urls from cms
+  const response = await fetch(`${config.apiUrl}/posts/published`);
+
+  const data = await response.json();
+  const fields = data.map((item) => ({
+    loc: `${process.env.SITE_URL || 'https://solodevhub.com'}/posts/${item.slug}`,
+    lastmod: item.updatedAt,
+    priority: 0.8,
+    changefreq: "monthly",
+  }));
+
+  return getServerSideSitemapLegacy(ctx, fields)
+}
+
+// Default export to prevent next.js errors
+export default function SitemapIndex() {}
