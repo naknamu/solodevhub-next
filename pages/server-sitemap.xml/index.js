14f749caa562a4805a3e8620cd8d4799f53952fc
naknamu <jodeldelvalle@gmail.com>
2023-06-10 17:45:27 +0800

    Uncomment dynamic sitemap

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,21 +1,21 @@
-// // pages/server-sitemap.xml/index.tsx
-// import { getServerSideSitemapLegacy } from 'next-sitemap'
-// import config from "@/config/config";
+// pages/server-sitemap.xml/index.tsx
+import { getServerSideSitemapLegacy } from 'next-sitemap'
+import config from "@/config/config";
 
-// export const getServerSideProps = async (ctx) => {
-//   // Method to source urls from cms
-//   const response = await fetch(`${config.apiUrl}/posts/published`);
+export const getServerSideProps = async (ctx) => {
+  // Method to source urls from cms
+  const response = await fetch(`${config.apiUrl}/posts/published`);
 
-//   const data = await response.json();
-//   const fields = data.map((item) => ({
-//     loc: `${process.env.SITE_URL || 'https://solodevhub.com'}/posts/${item.slug}`,
-//     lastmod: item.updatedAt,
-//     priority: 0.8,
-//     changefreq: "monthly",
-//   }));
+  const data = await response.json();
+  const fields = data.map((item) => ({
+    loc: `${process.env.SITE_URL || 'https://solodevhub.com'}/posts/${item.slug}`,
+    lastmod: item.updatedAt,
+    priority: 0.8,
+    changefreq: "monthly",
+  }));
 
-//   return getServerSideSitemapLegacy(ctx, fields)
-// }
+  return getServerSideSitemapLegacy(ctx, fields)
+}
 
-// // Default export to prevent next.js errors
-// export default function SitemapIndex() {}
+// Default export to prevent next.js errors
+export default function SitemapIndex() {}
