8c4163fd548b10146e0e77cb0b94c1da98d8a80a
naknamu <jodeldelvalle@gmail.com>
2023-06-12 10:04:20 +0800

    Added gtag code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { useRouter } from "next/router";
-import { Montserrat, Inter } from "next/font/google";
-import Head from "next/head";
+import { Montserrat } from "next/font/google";
+import Script from "next/script";
 
 const mont = Montserrat({ subsets: ["latin"] });
 
@@ -12,6 +12,22 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={mont.className}>
       <Layout>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YNVGXK4VGH"
+        ></Script>
+        <Script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+
+          gtag("config", "G-YNVGXK4VGH");
+          `}
+        </Script>
         <Component {...pageProps} key={router.asPath} />
       </Layout>
     </main>
