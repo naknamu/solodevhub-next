cacf0f66608862ada09bb882d17760e672341f04
naknamu <jodeldelvalle@gmail.com>
2023-10-18 13:38:30 +0800

    Changed also the link for Category button to enable right clicking

--- components/CategoryButton.js
+++ components/CategoryButton.js
@@ -1,15 +1,18 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CategoryButton = ({ category }) => {
   const router = useRouter();
 
-  const handleClick = () => {
-    router.push(`/categories/${category.slug}/`);
-  };
+  // const handleClick = () => {
+  //   router.push(`/categories/${category.slug}/`);
+  // };
 
   return (
-    <button className="blog-category text-tiny" onClick={handleClick}>
-      {category.name}
+    <button className="blog-category text-tiny">
+      <Link href={`/categories/${category.slug}`}>
+        {category.name}
+      </Link>
     </button>
   );
 };
