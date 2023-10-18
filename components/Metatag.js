f7e3c9c9addc92eaa016a281f670fa24c1e361ea
naknamu <jodeldelvalle@gmail.com>
2023-10-18 14:27:45 +0800

    Added coninicalURL

--- components/Metatag.js
+++ components/Metatag.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Metatag = ({ title }) => {
+  const router = useRouter();
+  const canonicalUrl = (`https://solodevhub.com/` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
+
   return (
     <Head>
+      <link rel="canonical" href={canonicalUrl} />
       <title>{`${title} | Solo Dev Hub`}</title>
     </Head>
   );
